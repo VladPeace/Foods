9059f75218c8b89c872840bb7c5d6e4ea93cd7b2
Vlad <vladpeace.peace@gmail.com>
2023-11-21 16:18:23 +0200

    Удаление блока с меню (создаем ее в js)

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 import { initializeTabs } from "./tabs/tabs.js";
 import { timerDeadLine } from "./timer/timer.js";
 import { modalWindow } from "./modal/modal.js";
+import { classMenu } from "./menu/classMenu.js";
 
 document.addEventListener('DOMContentLoaded', () => {
 	initializeTabs();
 	timerDeadLine();
 	modalWindow();
+	classMenu();
 });
+
