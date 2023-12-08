734ea814a05c149788f754bfc2fefe83502e05ba
vlad peace <vladpeace.peace@gmail.com>
2023-12-08 17:33:35 +0200

    Закончил первый вариант реализации слайдера

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 import { initializeTabs } from "./tabs/tabs.js";
 import { timerDeadLine } from "./timer/timer.js";
-import { modalWindow} from "./modal/modal.js";
+import { modalWindow } from "./modal/modal.js";
 import { classMenu } from "./menu/classMenu.js";
+import { slider } from "./slider/slider.js";
 
 document.addEventListener('DOMContentLoaded', () => {
 	initializeTabs();
 	timerDeadLine();
 	modalWindow();
 	classMenu();
+	slider();
 });
 
--- js/slider/slider.js
+++ js/slider/slider.js
@@ -1,3 +1,50 @@
 export function slider() {
+	// 1. Получаем элементы с которыми будем работать
+	const slides = document.querySelectorAll('.offer__slide');
+	const prev = document.querySelector('.offer__slider-prev');
+	const next = document.querySelector('.offer__slider-next');
+	const total = document.querySelector('#total');
+	const current = document.querySelector('#current');
 
+	// 2. Параметр (индекс) который определяет наш текущий слайд
+	let slideIndex = 1;
+
+	// 2.1 Инициализируем нашу функцию
+	showSlides(slideIndex);
+
+	if (slides.length < 10) {
+		total.textContent = `0${slides.length}`;
+	} else {
+		total.textContent = slides.length;
+	}
+
+	// 3. Показ текущего слайда и скрытие остальных
+	function showSlides(n) {
+		if (n > slides.length) {
+			slideIndex = 1;
+		}
+		if (n < 1) {
+			slideIndex = slides.length;
+		}
+		slides.forEach(item => item.style.display = "none");
+		slides[slideIndex - 1].style.display = "block";
+
+		if (slides.length < 10) {
+			current.textContent = `0${slideIndex}`;
+		} else {
+			current.textContent = slideIndex;
+		}
+	}
+
+	function plusSlides(n) {
+		showSlides(slideIndex += n);
+	}
+
+	// 4. Вешаем наши функции через обработчик событий
+	prev.addEventListener('click', () => {
+		plusSlides(-1);
+	});
+	next.addEventListener('click', () => {
+		plusSlides(1);
+	});
 }
